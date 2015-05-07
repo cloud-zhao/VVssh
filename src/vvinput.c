04d29e85bf0a57dc3d89f7ede222aefc46141ada
cloud-zhao <wlsdhzy@163.com>
2015-05-07 12:32:20 +0800

    new vvinput

--- src/vvinput.c
+++ src/vvinput.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
+#include <unistd.h>
+#include <pwd.h>
 #include "../lib/vvsqlite.h"
 
 int main(int argc,char **argv){
@@ -16,7 +18,9 @@ int main(int argc,char **argv){
 	
 
 	FILE *fp=NULL;
-	char *example="hostname|ip|user|password|role\n\tor use /** Notes";
+	char *example="hostname|ip|user|password|private key|role\n\tor use /** Notes\nEx:\n\t"
+		"localhost|127.0.0.1|root|123456|null|null\n\t"
+		"localhost|127.0.0.1|root|null|/root/.ssh/id_rsa|null";
 	if((fp=fopen(filename,"r"))==NULL){
 		fprintf(stderr,"Cannot open file:%s\n",filename);
 		return 1;
@@ -25,8 +29,8 @@ int main(int argc,char **argv){
 	char *content=(char*)malloc(sizeof(char)*1024);
 	char *hostinfo[10];
 	int i,len;
-	for(i=0;i<5;i++)
-		hostinfo[i]=(char*)malloc(sizeof(char));
+	for(i=0;i<6;i++)
+		hostinfo[i]=(char*)malloc(sizeof(char)*100);
 
 	while((fgets(content,1024,fp))!=NULL){
 		len=strlen(content);
@@ -35,7 +39,7 @@ int main(int argc,char **argv){
 			continue;
 		char *substr=strtok(content,"|");
 		if(substr==NULL){
-			fprintf(stderr,"file content format error:\n\t%s\n",example);
+			fprintf(stderr,"file content format error:\n%s\n",example);
 			continue;
 		}	
 		i=0;
@@ -43,20 +47,40 @@ int main(int argc,char **argv){
 			strcpy(hostinfo[i++],substr);
 			substr=strtok(NULL,"|");
 		}
-		if(i<3){
-			fprintf(stderr,"file content format error:\n\t%s\n",example);
-			continue;
+		if((i!=6) || (!strcmp(hostinfo[0],"null")) || (!strcmp(hostinfo[1],"null"))){
+			fprintf(stderr,"file content format error:\n%s\n",example);
+			return 0;
 		}
-		if(i==3)
-			strcpy(hostinfo[4],"default");
-		if(sqlite3_insert(hostinfo[0],hostinfo[1],hostinfo[2],hostinfo[3],hostinfo[4]))
+		if(!strcmp(hostinfo[2],"null")){
+			struct passwd *pwd;
+			pwd=getpwuid(getuid());
+			strcpy(hostinfo[2],pwd->pw_name);
+		}
+		if(!strcmp(hostinfo[3],"null")){
+			strcpy(hostinfo[3],"null");
+		}
+		if(!strcmp(hostinfo[4],"null")){
+			if(!strcmp(hostinfo[2],"root"))
+				strcpy(hostinfo[4],"/root/.ssh/id_rsa");
+			else{
+				strcpy(hostinfo[4],"/home/");
+				strcat(hostinfo[4],hostinfo[2]);
+				strcat(hostinfo[4],"/.ssh/id_rsa");
+			}
+		}
+		if(!strcmp(hostinfo[5],"null")){
+			strcpy(hostinfo[5],"defalut");
+		}
+		
+		if(sqlite3_insert(hostinfo[0],hostinfo[1],hostinfo[2],hostinfo[3],hostinfo[4],
+					hostinfo[5]))
 			fprintf(stderr,"Error: insert data failed\n");
 		else
-			printf("%s\t%s\t%s\t%s\t%s\tinsert successery!!!\n",hostinfo[0],hostinfo[1]
-				,hostinfo[2],hostinfo[3],hostinfo[4]);
+			printf("%s\t%s\t%s\t%s\t%s\t%s\tinsert successery!!!\n",hostinfo[0],hostinfo[1]
+				,hostinfo[2],hostinfo[3],hostinfo[4],hostinfo[5]);
 	}
 
-	for(i=0;i<5;i++)
+	for(i=0;i<6;i++)
 		free(hostinfo[i]);
 	free(content);
 	fclose(fp);
