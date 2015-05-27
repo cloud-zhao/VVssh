4fe80ff0baf5bd23a684b30cd61aa7b1d60ea0eb
cloud-zhao <wlsdhzy@163.com>
2015-05-27 11:12:59 +0800

    update head

---
+++ lib/vvhead.h
@@ -0,0 +1,51 @@
+#ifndef _SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+#ifndef _NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#ifndef _UNISTD_H
+#include <unistd.h>
+#endif
+#ifndef _ARPA_INET_H
+#include <arpa/inet.h>
+#endif
+#ifndef _SYS_TIME_H
+#include <sys/time.h>
+#endif
+#ifndef _SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifndef _FCNTL_H
+#include <fcntl.h>
+#endif
+#ifndef _ERRNO_H
+#include <errno.h>
+#endif
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+#ifndef _CTYPE_H
+#include <ctype.h>
+#endif
+#ifndef _GETOPT_H
+#include <getopt.h>
+#endif
+#ifndef _DIRENT_H
+#include <dirent.h>
+#endif
+#ifndef _SYS_STAT_H
+#include <sys/stat.h>
+#endif
+#ifndef _STRING_H
+#include <string.h>
+#endif
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#endif
+#ifndef _PWD_H
+#include <pwd.h>
+#endif
+#ifndef _MALLOC_H
+#include <malloc.h>
+#endif
